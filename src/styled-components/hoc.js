0d4091091034e1b83133960ccdcd041dfd18d2ee
Brent Jackson <jxnblk@gmail.com>
2017-06-18 11:17:33 -0400

    Add color util to HOCs

--- src/styled-components/hoc.js
+++ src/styled-components/hoc.js
@@ -2,12 +2,14 @@ const styled = require('styled-components').default
 const fontSize = require('../font-size')
 const width = require('../width')
 const space = require('../space')
+const color = require('../color')
 
 module.exports = Component => {
   const SystemComponent = styled(Component)`
     ${fontSize}
     ${width}
     ${space}
+    ${color}
   `
 
   return SystemComponent
