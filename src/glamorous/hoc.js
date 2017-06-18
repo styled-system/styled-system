cda0d9a55b2194c525230b0101fc10a41e8fa2e1
Brent Jackson <jxnblk@gmail.com>
2017-06-18 10:47:34 -0400

    Fix glamorous export

--- src/glamorous/hoc.js
+++ src/glamorous/hoc.js
@@ -1,4 +1,4 @@
-const glamorous = require('glamorous')
+const glamorous = require('glamorous').default
 const fontSize = require('../font-size')
 const width = require('../width')
 const space = require('../space')
