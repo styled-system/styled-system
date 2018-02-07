1368646c1e7815a52f64204fdfde0e888a61b855
John Otander <johnotander@gmail.com>
2018-02-06 21:10:43 -0700

    Run npm i if ci env

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,16 @@
 const { exec } = require('child_process')
 const { promisify } = require('util')
+const isCI = require('is-ci')
 const x = promisify(exec)
 
 const packages = require('./packages')
 
-const cmd = pkg => `cd ${pkg} && npm t`
+const cmd = pkg => `cd ${pkg} ${isCI ? '&& npm i': ''} && npm t`
+
 const runTest = async pkg => {
   try {
     const { stdout, stderr } = await x(cmd(pkg))
+
     if (stderr) console.error(stderr)
     if (stdout) console.log(stdout)
   } catch(err) {
