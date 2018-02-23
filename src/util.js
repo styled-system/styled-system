8bbb0e81469aa59da0ad03dd21da050b12d3bee7
Josh Rosenstein <josh@joshuarosenstein.com>
2018-02-23 12:25:22 -0500

    Add Optional props argument in Getter

--- src/util.js
+++ src/util.js
@@ -49,9 +49,9 @@ export const merge = (a, b) => Object.assign({}, a, b, Object.keys(b).reduce((ob
   }),
   {}))
 
-export const getValue = (val, getter, toPx) =>
+export const getValue = (val, getter, toPx,props) =>
   typeof getter === 'function'
-    ? getter(val)
+    ? getter(val,props)
     : toPx ? px(val) : val
 
 export const style = ({
@@ -70,7 +70,7 @@ export const style = ({
     const value = getValue(
       get(th, [ key, n ].join('.'), n),
       getter,
-      numberToPx
+      numberToPx,props
     )
 
     return { [cssProperty]: value }
