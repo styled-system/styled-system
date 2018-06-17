b36648e174316339b4e9b05178a8b74c0f826ec3
Brent Jackson <jxnblk@gmail.com>
2018-06-17 18:41:14 -0400

    Add meta tags

--- docs/_app.js
+++ docs/_app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'rebass'
 import { Head } from 'ok-docs'
 import theme from './_theme'
+import { description } from '../package.json'
 
 export default class extends React.Component {
   static defaultProps = {
@@ -26,6 +27,11 @@ export default class extends React.Component {
       <React.Fragment>
         <Head>
           <title>Styled System</title>
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:site' content='@jxnblk' />
+          <meta name='twitter:image' content='https://jxnblk.com/styled-system/logo.png' />
+          <meta name='twitter:title' content='Styled System' />
+          <meta name='twitter:description' content={description} />
         </Head>
         {children}
       </React.Fragment>
