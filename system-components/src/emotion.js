0431b74020ea7a6547e70be2424857e64a95f9fb
tkh <kye.hohenberger@gmail.com>
2018-06-22 10:30:19 -0600

    Fix my misguided attempt.
    https://github.com/jxnblk/styled-system/pull/219/files#r197373080

--- system-components/src/emotion.js
+++ system-components/src/emotion.js
@@ -3,7 +3,9 @@ import System from './System'
 
 const create = new System({
   createComponent: type => (...args) =>
-    styled(type, { shouldForwardProp: name => name === "innerRef" })(...args)
+    styled(type, {
+      shouldForwardProp: name => name !== 'ref' && name !== 'theme'
+    })(...args)
 })
 
 export default create
