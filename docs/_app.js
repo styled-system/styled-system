ccb90cff6a724952197789b2b94eb285c44ea733
Brent Jackson <jxnblk@gmail.com>
2018-06-22 16:53:13 -0400

    Update docs for x0

--- docs/_app.js
+++ docs/_app.js
@@ -1,28 +1,81 @@
 import React from 'react'
-import { Provider } from 'rebass'
-import { Head } from 'ok-docs'
-import theme from './_theme'
+import { Provider, theme as rebassTheme } from 'rebass'
+import { SidebarLayout } from '@compositor/x0/components'
 import { description } from '../package.json'
+import Logo from './Logo'
+
+const theme = {
+  ...rebassTheme,
+  space: [
+    0, 4, 8, 16, 32, 64, 128, 256
+  ]
+}
+
+const navOrder = [
+  'Home',
+  'getting-started',
+  'responsive-styles',
+  'how-it-works',
+  'api',
+  'table',
+  'custom-props',
+  'troubleshooting',
+  'Logo',
+]
+
+const createNav = routes => [
+  ...[...routes]
+    .map(route => {
+      if (route.name !== 'index') return route
+      return {
+        ...route,
+        name: 'Home'
+      }
+    })
+    .sort((a, b) => {
+      const ai = navOrder.indexOf(a.name)
+      const bi = navOrder.indexOf(b.name)
+      if (ai < 0) return 1
+      return ai - bi
+    }),
+  {
+    key: 'github',
+    name: 'GitHub',
+    path: 'https://github.com/jxnblk/styled-system',
+  },
+  {
+    key: 'jxnblk',
+    name: 'Made by Jxnblk',
+    path: 'https://jxnblk.com',
+  },
+]
 
 export default class extends React.Component {
   static defaultProps = {
-    theme,
     title: 'Styled System',
-    navOrder: [
-      'Home',
-      'getting-started',
-      'responsive-styles',
-      'how-it-works',
-      'API',
-      'table',
-      'custom-props',
-      'troubleshooting',
-      'Logo',
-    ]
   }
 
   render () {
-    const { children } = this.props
+    const { route, routes, children } = this.props
+    const { layout } = (route && route.props) || {}
+    const nav = createNav(routes)
+
+    return (
+      <Provider theme={theme}>
+        {layout === false ? children : (
+          <SidebarLayout
+            {...this.props}
+            routes={nav}
+            logo={<Logo size={32} />}
+          />
+        )}
+      </Provider>
+    )
+
+    if (layout === false) return children
+    return <SidebarLayout {...this.props} />
+
+    /*
     return (
       <React.Fragment>
         <Head>
@@ -36,5 +89,6 @@ export default class extends React.Component {
         {children}
       </React.Fragment>
     )
+    */
   }
 }
