082ada8df9b08d81aa99c1a4929b1b1916cfa2d3
Brent Jackson <jxnblk@gmail.com>
2018-08-23 13:37:30 -0400

    Adjust rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,34 +6,19 @@ import pkg from './package.json'
 const input = 'src/index.js'
 
 export default [
-  /* throws syntax error with `export * as`
-  {
-    input: 'src/index.js',
-    dest: pkg.browser,
-    format: 'umd',
-    moduleName: 'styled-system',
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        plugins: ['external-helpers'],
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-  */
   {
     input: 'src/index.js',
     external: [
       'prop-types'
     ],
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' }
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
     ],
     plugins: [
       babel({
-        exclude: ['node_modules/**']
+        exclude: ['node_modules/**'],
+        plugins: [ 'external-helpers' ]
       })
     ]
   }
