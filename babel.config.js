478a783445d4c5e26bb44bcef319af915a29fc72
Bogdan Chadkin <trysound@yandex.ru>
2018-09-14 00:39:54 +0300

    Add babel runtime

--- babel.config.js
+++ babel.config.js
@@ -5,16 +5,25 @@ module.exports = {
         [ '@babel/env', { loose: true } ],
         '@babel/react'
       ],
+      plugins: [
+        '@babel/transform-runtime'
+      ]
     },
     cjs: {
       presets: [
         [ '@babel/env', { loose: true } ]
       ],
+      plugins: [
+        '@babel/transform-runtime'
+      ]
     },
     esm: {
       presets: [
         [ '@babel/env', { loose: true, modules: false } ]
       ],
+      plugins: [
+        ['@babel/transform-runtime', { useESModules: true }]
+      ]
     },
   }
 }
