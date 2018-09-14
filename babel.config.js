114e8f459361f1d9af65a409ceadac444bd7b8de
Brent Jackson <jxnblk@gmail.com>
2018-09-14 00:15:38 -0400

    Simplify build

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,6 @@
 module.exports = {
-  env: {
-    test: {
-      presets: [
-        '@babel/env',
-        '@babel/react'
-      ],
-    },
-    cjs: {
-      presets: [
-        [ '@babel/env', { loose: true } ]
-      ],
-    },
-    esm: {
-      presets: [
-        [ '@babel/env', { loose: true, modules: false } ]
-      ],
-    },
-  }
+  presets: [
+    [ '@babel/env', { loose: true } ],
+    '@babel/react'
+  ]
 }
