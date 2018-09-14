bb396a7067a62b9fe2ebbc717f2a76ee315373dd
Bogdan Chadkin <trysound@yandex.ru>
2018-09-14 18:30:22 +0300

    Remove babel runtime

--- babel.config.js
+++ babel.config.js
@@ -4,25 +4,16 @@ module.exports = {
       presets: [
         [ '@babel/env', { loose: true } ],
         '@babel/react'
-      ],
-      plugins: [
-        '@babel/transform-runtime'
       ]
     },
     cjs: {
       presets: [
         [ '@babel/env', { loose: true } ]
-      ],
-      plugins: [
-        '@babel/transform-runtime'
       ]
     },
     esm: {
       presets: [
         [ '@babel/env', { loose: true, modules: false } ]
-      ],
-      plugins: [
-        ['@babel/transform-runtime', { useESModules: true }]
       ]
     },
   }
