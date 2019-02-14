afb23cb3906fcf35fa25a4d42b480762eda0fbd3
Brent Jackson <jxnblk@gmail.com>
2019-02-14 15:06:39 -0500

    Add svg pattern for docs

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { Box, Flex, Container, Text, Blockquote, Link, UL } from '../components'
+import { Box, Flex, Container, Text, Blockquote, Link, UL, Pre } from '../components'
 import Logo from '../Logo'
 import Badges from '../badges.md'
 import CodeSandbox from '../CodeSandbox'
@@ -8,6 +8,7 @@ import GettingStarted from '../../getting-started.md'
 import Docs from '../../README.md'
 import Footer from '../Footer'
 // import Pattern from '../Pattern'
+import Hex from '../Hex'
 
 export const query = graphql`
   query Index {
@@ -35,33 +36,30 @@ export default ({
 }) =>
   <div>
     <Box as='header'
-      px={4}
       py={[ 5, 6 ]}
       color='black'
       bg='lavender'>
-      <Flex>
-        <Logo />
-        <Logo />
-        <Logo />
-        <Logo />
-      </Flex>
-      <Flex mb={5}>
-        <Logo />
-        <Logo />
-        <Logo />
-        <Logo />
-      </Flex>
-      <Text as='h1'
-        fontSize={[6]}
-        lineHeight='1.125'>
-        {meta.title}
-      </Text>
-      <Text fontSize={[3, 4, 5]} fontWeight='bold'>
-        {meta.description}
-      </Text>
-      <Badges />
+      <Container>
+        <Text as='h1'
+          fontSize={[5]}
+          mb={4}
+          lineHeight='1.125'>
+          {meta.title}
+        </Text>
+        <Hex />
+        <Text fontSize={[3, 4, 5]} fontWeight='bold' mt={4}>
+          {meta.description}
+        </Text>
+        <Pre
+          px={0}
+          color='inherit'
+          backgroundColor='transparent'>
+          npm i styled-system
+        </Pre>
+      </Container>
     </Box>
     <Container py={5}>
+      <Badges />
       <Box py={4}>
         <UL>
           {meta.features.map(feature => (
