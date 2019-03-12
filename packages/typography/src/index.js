606b9b7cc619536b8063c337ac1b4830cbeb1af5
Brent Jackson <jxnblk@gmail.com>
2019-03-12 12:27:01 -0400

    Add scoped option to typography

--- packages/typography/src/index.js
+++ packages/typography/src/index.js
@@ -56,16 +56,23 @@ export const typographyStyles = compose(
   color
 )
 
+const getScopedSelector = key => {
+  return key.split(',').map(k => '& ' + key).join(', ')
+}
+
 export const typography = props => {
   // handle emotion's Global and css props
   const theme = props.theme || props
+  theme.typography = theme.typography || {}
   const styles = {}
+  const scoped = theme.typography.scoped
   const elements = pick(theme.typography, tagNames)
   for (const key in elements) {
     const el = elements[key]
     const rules = typographyStyles({ theme, ...el })
     // flatten & merge??
-    styles[key] = [ el, ...rules ]
+    const selector = scoped ? getScopedSelector(key) : key
+    styles[selector] = [ el, ...rules ]
   }
 
   return styles
