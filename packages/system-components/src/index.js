c2c87515e335f8386e120a04355a48fcd77cc8bf
Brent Jackson <jxnblk@gmail.com>
2019-03-12 11:20:37 -0400

    Remove deprecated system-components package

--- packages/system-components/src/index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import System from './System'
-
-const create = new System({
-  createComponent: type => (...args) => styled(type)([], ...args)
-})
-
-export default create
-export { default as System } from './System'
