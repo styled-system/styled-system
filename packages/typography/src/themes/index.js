76bd52bfeb9cebaa0dfbb825996ac45ea77b69f0
Brent Jackson <jxnblk@gmail.com>
2019-03-23 17:51:59 -0400

    Remove packages in favor of separate repo

--- packages/typography/src/themes/index.js
+++
@@ -1,5 +0,0 @@
-export { baskerville } from './baskerville'
-export { future } from './future'
-export { modern } from './modern'
-export { poppins } from './poppins'
-export { roboto } from './roboto'
