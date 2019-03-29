3533000a748bacf580a90e7c72f71588c435cb6c
Brent Jackson <jxnblk@gmail.com>
2019-03-29 15:08:27 -0400

    Add MDX TOC

--- docs/api.md
+++ docs/api.md
@@ -1,4 +1,6 @@
 
+export const toc = true
+
 # API
 
 ## Core
--- docs/src/toc-wrapper.js
+++ docs/src/toc-wrapper.js
@@ -1,25 +1,66 @@
 import React from 'react'
+import { Box, Styled } from './system'
 
 const headings = ['h2', 'h3']
 const getLinks = children => React.Children.toArray(children)
   .reduce((a, child) => {
     if (!headings.includes(child.props.mdxType)) return a
+    const item = {
+      text: child.props.children,
+      href: '#' + child.props.id
+    }
+    const level = headings.indexOf(child.props.mdxType)
+    if (level === 0) {
+      return [ ...a, item ]
+    }
+    const previous = a[a.length - 1]
+    if (Array.isArray(previous)) {
+      return [
+        ...a.slice(0, a.length - 1),
+        [ ...previous, item ]
+      ]
+    }
     return [
       ...a,
-      {
-        level: headings.indexOf(child.props.mdxType),
-        text: child.props.children,
-        href: '#' + child.props.id
-      }
+      [ item ]
     ]
   }, [])
 
+const List = ({ links }) =>
+  <Styled.ul
+    pl={2}
+    css={{
+      listStyle: 'none',
+    }}>
+    {links.map((link, i) => Array.isArray(link) ? (
+      <List key={i} links={link} />
+    ) : (
+      <Styled.li key={link.href}>
+        <Styled.a href={link.href}>
+          {link.text}
+        </Styled.a>
+      </Styled.li>
+    ))}
+  </Styled.ul>
+
+const TOC = ({ links }) =>
+  <Box
+    px={2}
+    ml={2}
+    fontSize={1}
+    css={{
+      float: 'right',
+    }}>
+    <List links={links} />
+  </Box>
 
 export default props => {
+  if (!props.toc) return <>{props.children}</>
+
   const links = getLinks(props.children)
-  // console.log(links)
   return (
     <>
+      <TOC links={links} />
       {props.children}
     </>
   )
