a57cceef8f41125b37f7939b67505d19b91006e8
Brent Jackson <jxnblk@gmail.com>
2019-05-27 21:09:15 -0400

    Simplify build

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,5 @@
 module.exports = {
-  env: {
-    test: {
-      presets: [
-        [ '@babel/env', { loose: true } ],
-        '@babel/react'
-      ],
-      plugins: [
-        '@babel/transform-runtime'
-      ]
-    },
-    cjs: {
-      presets: [
-        [ '@babel/env', { loose: true } ]
-      ],
-      plugins: [
-        '@babel/transform-runtime'
-      ]
-    },
-    esm: {
-      presets: [
-        [ '@babel/env', { loose: true, modules: false } ]
-      ],
-      plugins: [
-        ['@babel/transform-runtime', { useESModules: true }]
-      ]
-    },
-  }
+  presets: [
+    '@babel/preset-env'
+  ],
 }
