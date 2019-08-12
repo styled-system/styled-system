a341278ef358b4802a67e1a66e793e3ed7ff49b8
Brent Jackson <jxnblk@gmail.com>
2019-08-12 13:10:06 -0400

    Add placeholder for treat example

---
+++ examples/treat/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = props => {
+
+  return (
+    <div>
+      <h1>Treat example</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
