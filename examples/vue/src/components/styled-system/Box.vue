8574dfd06cbd00ebae96aadc45851742277deb6f
Phil Andrews <phil@frontside.com.au>
2020-10-31 12:05:42 +1100

    add Vue js example

---
+++ examples/vue/src/components/styled-system/Box.vue
@@ -0,0 +1,31 @@
+<template functional>
+  <component :is="$options.components.StyledBox" v-bind="props">
+    <slot />
+  </component>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+import { space, fontSize, color } from 'styled-system'
+import propTypes from './propTypes'
+
+const props = {
+  ...propTypes.space,
+  ...propTypes.typography,
+  ...propTypes.color
+}
+
+const StyledBox = styled('div', props)`
+  ${space}
+  ${fontSize}
+  ${color}
+`
+
+export default {
+  name: 'Box',
+  components: {
+    StyledBox
+  },
+  props
+}
+</script>
