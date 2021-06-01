0d92c0bff6b3cae926ec9bd8fc0fec1c641914d1
viniarruda <viniarruda.souza@gmail.com>
2021-06-01 18:16:02 -0300

    fix: resolve build problems

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
+
+export default {
+  input: 'src/styled-system/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  external: ['styled-components'],
+  plugins: [
+    resolve(),
+    commonjs(),
+  ]
+};
