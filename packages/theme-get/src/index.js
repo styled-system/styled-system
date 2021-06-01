7f7846bfbc672bd0a787e04bd3d02321f5152162
viniarruda <viniarruda.souza@gmail.com>
2021-06-01 15:36:06 -0300

    fix: remove workspaces

--- packages/theme-get/src/index.js
+++
@@ -1,5 +0,0 @@
-import { get } from '@styled-system/core'
-
-export const themeGet = (path, fallback = null) => props =>
-  get(props.theme, path, fallback)
-export default themeGet
---
+++ src/shadow/src/index.js
@@ -0,0 +1,14 @@
+import { system } from '../core'
+
+export const shadow = system({
+  boxShadow: {
+    property: 'boxShadow',
+    scale: 'shadows',
+  },
+  textShadow: {
+    property: 'textShadow',
+    scale: 'shadows',
+  },
+})
+
+export default shadow
